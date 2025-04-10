709b9e1f64da2d6201c28cdf636b4cd132b5492a
chris985 <christianlonardoni@gmail.com>
2025-04-10 14:11:40 -0300

    commit

--- model.py
+++ model.py
@@ -1,23 +1,57 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base,
+from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base, relationship
 
 engine = create_engine('sqlite:///nome.sqlite3')
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 # Base_declarativa - Ela permite que você defina classes Python que representam tabelas de
 # banco de dados de forma declarativa, sem a necessidade de configurar manualmente a
 # relação entre as classes e as tabelas.
-Base.query = db_session.query_property()
+
+class Ativadade(Base):
+    __tablename__ = 'ativadade'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
+    pessoas = relationship('Pessoa')
+
 class Veiculo():
     __tablename__ = 'Veiculos'
-    id = db.Collumn(db.Integer, primary_key=True)
-    name = db.Collumn(db.String(100), nullable=False)
-    marca = db.Collumn(db.String(100), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False, index=True)
+    marca = Column(String(100), nullable=False, index=True)
 
 class Cliente():
     __tablename__ = 'clientes'
-    id = db.Collumn(db.Integer, primary_key=True)
-    nome = db.Collumn(db.String(100), nullable=False)
-    email = db.Collumn(db.String(100), nullable=False)
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+
+# Função para salvar no banco
+def save(self):
+    db_session.add(self)
+    db_session.commit()
+
+# Função para deletar
+def delete(self):
+    db_session.delete(self)
+    db_session.commit()
+
+def serialize_user(self):
+    dados_user = {
+        'id': self.id,
+        'nome': self.nome,
+        'email': self.email,
+        'marca': self.marca
+    }
+    return dados_user
+
+# Método para criar banco
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    init_db()
