1c2a4891bdca7c3eca3dec91d5a29b0d2d90893e
chris985 <christianlonardoni@gmail.com>
2025-04-10 15:52:07 -0300

    commit

--- models.py
+++ models.py
@@ -1,45 +1,41 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base, relationship
 
+# Configuração com a conexão com o banco de dados
 engine = create_engine('sqlite:///atividades.sqlite3')
+
+# Gerencia as sessões com o banco de dados
 db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-# Método para criar banco
-def init_db():
-    Base.metadata.create_all(bind=engine)
 
-if __name__ == '__main__':
-    init_db()
 
 # Base_declarativa - Ela permite que você defina classes Python que representam tabelas de
 # banco de dados de forma declarativa, sem a necessidade de configurar manualmente a
 # relação entre as classes e as tabelas.
 
-class Ativadade(Base):
-    __tablename__ = 'ativadades'
-    id = Column(Integer, primary_key=True)
-    nome = Column(String(80))
-    cliente_id = Column(Integer, ForeignKey('clientes.id'))
-    clientes = relationship('Cliente')
 
-class Veiculo():
+class Veiculo(Base):
     __tablename__ = 'Veiculos'
     id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False, index=True)
+    nome = Column(String(100), nullable=False, index=True)
+    modelo = Column(String(100), nullable=False, index=True)
+    placa = Column(String(100), nullable=False, index=True)
+    ano_fabricacao = Column(String(100), nullable=False, index=True)
     marca = Column(String(100), nullable=False, index=True)
 
-class Cliente():
+class Cliente(Base):
     __tablename__ = 'clientes'
     id = Column(Integer, primary_key=True)
-    nome = Column(String(100), nullable=False)
-    email = Column(String(100), nullable=False)
+    nome = Column(String(100), nullable=False, index=True)
+    email = Column(String(100), nullable=False, index=True)
+    endereco = Column(String(100), nullable=False, index=True)
 
     # Representação classe
     def __repr__(self):
-        return '<Cliente: {} {}>'.format(self.nome, self.email)
+        return '<Cliente: {} {}>'.format(self.nome, self.email, self.endereco)
 
     # Função para salvar no banco
     def save(self):
@@ -53,9 +49,31 @@ class Cliente():
 
     def serialize_user(self):
         dados_user = {
-            'id': self.id,
+            'id_user': self.id,
             'nome': self.nome,
             'email': self.email,
         }
         return dados_user
 
+class Atividade(Base):
+    __tablename__ = 'atividades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    cliente_id = Column(Integer, ForeignKey('clientes.id'))
+    clientes = relationship('Cliente')
+
+class Ordens_servicos(Base):
+    __tablename__ = 'ordens e servicos'
+    id = Column(Integer, primary_key=True)
+    veiculo_associado = Column(String(100), nullable=False, index=True)
+    data_abertura = Column(String(100), nullable=False, index=True)
+    descricao_servico = Column(String(100), nullable=False, index=True)
+    status = Column(String(100), nullable=False, index=True)
+    valor_estimado = Column(Integer, nullable=False, index=True)
+
+# Método para criar banco
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    init_db()
